c12a2a3fe24271708ee01c3020a765fc0152064e
Chris Speck <cgspeck@gmail.com>
2022-06-09 23:16:04 +1000

    Remove unneeded application object, remove default secret key, set a redirect url after login

--- src/app.py
+++ src/app.py
@@ -3,10 +3,13 @@ from flask import Flask, redirect, url_for
 from flask_dance.contrib.google import make_google_blueprint, google
 
 app = Flask(__name__)
-app.secret_key = os.environ.get("FLASK_SECRET_KEY", "supersekrit")
+app.secret_key = os.environ["FLASK_SECRET_KEY"]
 app.config["GOOGLE_OAUTH_CLIENT_ID"] = os.environ.get("GOOGLE_OAUTH_CLIENT_ID")
 app.config["GOOGLE_OAUTH_CLIENT_SECRET"] = os.environ.get("GOOGLE_OAUTH_CLIENT_SECRET")
-google_bp = make_google_blueprint(scope=["profile", "email"])
+google_bp = make_google_blueprint(
+    scope=["profile", "email"],
+    redirect_url=os.environ.get('APPLICATION_ROOT', None)
+)
 app.register_blueprint(google_bp, url_prefix="/login")
 app.config["AUTHORIZED_USERS"] = [e.lower() for e in os.environ.get('AUTHORIZED_USERS', '').split(',')]
 
@@ -24,5 +27,3 @@ def index():
         authorized_msg="" if authorized else "not",
         email=resp.json()["email"]
     )
-
-application = app
