1b9f12fe95284a7506315da3758848588f32590c
Chris Speck <cgspeck@gmail.com>
2025-04-19 09:23:53 +1000

    Make header validation a little easier to follow

--- src/controllers/upload_scores.py
+++ src/controllers/upload_scores.py
@@ -1,26 +1,44 @@
 from dataclasses import dataclass
 from typing import Any, Generator, List, Optional, Union
-from flask import Blueprint, Response, abort, current_app, g, render_template, request, url_for
+from flask import (
+    Blueprint,
+    Response,
+    abort,
+    current_app,
+    g,
+    render_template,
+    request,
+    url_for,
+)
 from flask_wtf import FlaskForm  # type: ignore
-from flask_wtf.file import (FileAllowed, FileField,  # type: ignore
-                            FileRequired, FileSize)
+from flask_wtf.file import (
+    FileAllowed,
+    FileField,  # type: ignore
+    FileRequired,
+    FileSize,
+)
 from wtforms import BooleanField, RadioField  # type: ignore
 from wtforms.validators import DataRequired  # type: ignore
 from mysql.connector import MySQLConnection
 
-from src.controllers.helpers import (backup_and_clear_scores,
-                                     db_config_for_env_shortname,
-                                     get_db_connection)
-from src.csv_validate import (REQUIRED_HEADERS, has_required_headers, is_csv,
-                              try_decode_stream)
+from src.controllers.helpers import (
+    backup_and_clear_scores,
+    db_config_for_env_shortname,
+    get_db_connection,
+)
+from src.csv_validate import (
+    REQUIRED_HEADERS,
+    validate_headers,
+    is_csv,
+    try_decode_stream,
+)
 from src.datadefs import ScoreEntry
 from src.email import EmailReason, send_audit_email
 from src.models.brewers import get_brewer_dict
 from src.models.judging_scores import check_create_westgate_fields
 from src.models.special_best_data import set_special_best_winner
 from src.place_getter import determine_place_getters
-from src.score_process import (load_entries_from_csv, prepare_entries,
-                               save_entries)
+from src.score_process import load_entries_from_csv, prepare_entries, save_entries
 from src.utils import must_be_authorized, save_upload
 
 
@@ -50,16 +68,20 @@ MESSAGES_FOOTER = """
 </html>
 """
 
+
 @dataclass
 class DeterminePlaceGetterReq:
     required_places: int
     category: Optional[str]
     category_name: str
 
+
 upload_scores = Blueprint("upload_scores", __name__, template_folder="templates")
+
 
 def display_message(message: str) -> str:
     return f"<p>{message}</p>"
+
 
 class UploadScoreForm(FlaskForm):
     environment = RadioField(
@@ -77,7 +99,7 @@ class UploadScoreForm(FlaskForm):
     confirm = BooleanField(
         "Confirm",
         description="All existing scores will be cleared and you will have to login to BCOE&M to manually set place-getters.",
-        validators=[DataRequired()]
+        validators=[DataRequired()],
     )
 
 
@@ -106,93 +128,116 @@ def show_form() -> Union[str, Response]:
             form.csv_file.errors.append("Does not appear to be a CSV")
             ok = False
 
-        if t_io and not has_required_headers(t_io):
-            form.csv_file.errors.append(f"Missing required headers: {REQUIRED_HEADERS}")
-            ok = False
-
         if not ok:
             return render_template(f"upload_scores_form.html", form=form)
 
-        homepage_path = url_for('homepage.show')
+        vhr = validate_headers(t_io)
+        if not vhr.ok:
+            form.csv_file.errors.append(f"Missing required headers: {vhr.missing}")
+            return render_template(f"upload_scores_form.html", form=form)
+
+        homepage_path = url_for("homepage.show")
 
         user_name = g.user_name
         user_email = g.user_email
-        bootstrap_css = url_for('static', filename='css/bootstrap.min.css')
-        bootstrap_js = url_for('static', filename='js/bootstrap.min.js')
+        bootstrap_css = url_for("static", filename="css/bootstrap.min.css")
+        bootstrap_js = url_for("static", filename="js/bootstrap.min.js")
 
         messages: List[str] = [MESSAGES_HEADER.format(bootstrap_css=bootstrap_css)]
         env_short_name = form.environment.data
         all_res_link = url_for("all_results.show", comp_env=env_short_name)
-        env_full_name = [x[1] for x in current_app.config["BCOME_ENV_CHOICES"] if x[0] == env_short_name][0]
+        env_full_name = [
+            x[1]
+            for x in current_app.config["BCOME_ENV_CHOICES"]
+            if x[0] == env_short_name
+        ][0]
         db_config = db_config_for_env_shortname(env_short_name, messages)
         remote_addr = request.remote_addr
 
         def stream_process_csv() -> Generator[str, Any, None]:
             for m in messages:
-                yield(display_message(m))
+                yield (display_message(m))
             messages.clear()
 
             if t_io is None:
                 abort(500, "Expected a stream")
 
-            filename = save_upload(t_io, user_name, user_email, env_short_name, remote_addr)
-            yield(display_message(f"Saved upload to {filename}"))
+            filename = save_upload(
+                t_io, user_name, user_email, env_short_name, remote_addr
+            )
+            yield (display_message(f"Saved upload to {filename}"))
 
             entries: List[ScoreEntry] = []
 
             try:
                 entries = load_entries_from_csv(t_io)
             except Exception as e:
-                yield(f"Error loading CSV: {e}")
-                yield(MESSAGES_FOOTER.format(homepage_path=homepage_path, bootstrap_js=bootstrap_js))
+                yield (f"Error loading CSV: {e}")
+                yield (
+                    MESSAGES_FOOTER.format(
+                        homepage_path=homepage_path, bootstrap_js=bootstrap_js
+                    )
+                )
                 return
 
-            yield(display_message(f"Loaded entries from CSV"))
+            yield (display_message(f"Loaded entries from CSV"))
 
             cnn: Optional[MySQLConnection] = None
 
             try:
                 cnn = get_db_connection(db_config)
             except Exception as e:
-                yield(f"Error connecting to database: {e}")
-                yield(MESSAGES_FOOTER.format(homepage_path=homepage_path, bootstrap_js=bootstrap_js))
+                yield (f"Error connecting to database: {e}")
+                yield (
+                    MESSAGES_FOOTER.format(
+                        homepage_path=homepage_path, bootstrap_js=bootstrap_js
+                    )
+                )
                 return
 
-            yield(display_message("Connected to DB."))
-            yield(display_message(f"Starting data validation..."))
+            yield (display_message("Connected to DB."))
+            yield (display_message(f"Starting data validation..."))
 
             data_valid = prepare_entries(cnn, entries, messages)
             for m in messages:
-                yield(display_message(m))
+                yield (display_message(m))
             messages.clear()
 
             if not data_valid:
-                yield(MESSAGES_FOOTER.format(homepage_path=homepage_path, bootstrap_js=bootstrap_js))
+                yield (
+                    MESSAGES_FOOTER.format(
+                        homepage_path=homepage_path, bootstrap_js=bootstrap_js
+                    )
+                )
                 return
 
-            yield(display_message("Data validation succeeded"))
-            yield(display_message("Starting backup..."))
+            yield (display_message("Data validation succeeded"))
+            yield (display_message("Starting backup..."))
             backup_and_clear_scores(cnn, env_short_name, messages)
             for m in messages:
-                yield(display_message(m))
+                yield (display_message(m))
             messages.clear()
 
-            yield(display_message("Checking database state..."))
+            yield (display_message("Checking database state..."))
 
             db_ok = check_create_westgate_fields(cnn, messages)
             for m in messages:
-                yield(display_message(m))
+                yield (display_message(m))
             messages.clear()
 
             if not db_ok:
-                yield(MESSAGES_FOOTER.format(homepage_path=homepage_path, bootstrap_js=bootstrap_js))
+                yield (
+                    MESSAGES_FOOTER.format(
+                        homepage_path=homepage_path, bootstrap_js=bootstrap_js
+                    )
+                )
                 return
 
-            yield(display_message(f"Sorting entries and determining place getters..."))
-            yield(display_message(f"{len(entries)} entries"))
+            yield (display_message(f"Sorting entries and determining place getters..."))
+            yield (display_message(f"{len(entries)} entries"))
             entries.sort(reverse=True)
 
-            yield(display_message("Loading brewer list..."))
+            yield (display_message("Loading brewer list..."))
             brewer_dict = get_brewer_dict(cnn)
 
             for e in entries:
@@ -246,13 +291,15 @@ def show_form() -> Union[str, Response]:
                             cnn, dpgr_res.place_getters[0], "Brewer of Show", messages
                         )
                 else:
-                    messages.append(f"Unable to set places for {dpgr.category_name}, unable to resolve count-back, must recall the judges")
+                    messages.append(
+                        f"Unable to set places for {dpgr.category_name}, unable to resolve count-back, must recall the judges"
+                    )
 
             for m in messages:
-                yield(display_message(m))
+                yield (display_message(m))
             messages.clear()
 
-            yield(display_message(f"Saving scores to database, please standby..."))
+            yield (display_message(f"Saving scores to database, please standby..."))
             save_entries(cnn, entries)
             messages.append(f"{len(entries)} scores saved to the database!")
             messages.append(f"<a href={all_res_link}>View all results here!</a>")
@@ -261,25 +308,33 @@ def show_form() -> Union[str, Response]:
             messages.append("You *MUST* go back to BCOE&M and do the following:")
             messages.append("")
             messages.append("1. manually resolve any place-setters mentioned above")
-            messages.append("2. identify and set the best novice (using the all results link above + notnovice spreadsheet), use judging number with leading zeros to set: https://comps.westgatebrewers.org/index.php?section=admin&go=special_best_data")
+            messages.append(
+                "2. identify and set the best novice (using the all results link above + notnovice spreadsheet), use judging number with leading zeros to set: https://comps.westgatebrewers.org/index.php?section=admin&go=special_best_data"
+            )
             messages.append("3. publish results")
             messages.append("")
-            messages.append("**Admin score reports will not work until place-getters are set**")
+            messages.append(
+                "**Admin score reports will not work until place-getters are set**"
+            )
             messages.append("-" * 20)
             messages.append("")
             messages.append("You may now close this tab")
-            messages.append(MESSAGES_FOOTER.format(homepage_path=homepage_path, bootstrap_js=bootstrap_js))
+            messages.append(
+                MESSAGES_FOOTER.format(
+                    homepage_path=homepage_path, bootstrap_js=bootstrap_js
+                )
+            )
 
             send_audit_email(
                 reason=EmailReason.UploadScores,
                 env_full_name=env_full_name,
                 user_name=user_name,
-                user_email=user_email
+                user_email=user_email,
             )
 
             for m in messages:
-                yield(display_message(m))
+                yield (display_message(m))
 
-        return current_app.response_class(stream_process_csv(), mimetype='text/html')
+        return current_app.response_class(stream_process_csv(), mimetype="text/html")
 
     return render_template(f"upload_scores_form.html", form=form)
--- src/csv_validate.py
+++ src/csv_validate.py
@@ -1,9 +1,17 @@
+from copy import deepcopy
 import csv
+from dataclasses import dataclass, field
 from io import StringIO
 import io
 from typing import Optional, TextIO
 
 REQUIRED_HEADERS = ["Entry Number", "Category", "Sub-category", "Total Score"]
+
+
+@dataclass
+class HeaderValidateResponse:
+    ok: bool = True
+    missing: list[str] = field(default_factory=list)
 
 
 def try_decode_stream(b: bytes) -> Optional[StringIO]:
@@ -16,19 +24,19 @@ def try_decode_stream(b: bytes) -> Optional[StringIO]:
     return t_io
 
 
-def has_required_headers(data: TextIO) -> bool:
+def validate_headers(data: TextIO) -> HeaderValidateResponse:
     dr = csv.DictReader(data)
     data.seek(0)
-    ok = True
+    required_headers = deepcopy(REQUIRED_HEADERS)
 
     if dr.fieldnames is None:
-        return False
+        return HeaderValidateResponse(ok=False, missing=required_headers)
 
-    for h in REQUIRED_HEADERS:
-        if h not in dr.fieldnames:
-            ok = False
+    [required_headers.remove(x) for x in dr.fieldnames if x in required_headers]
 
-    return ok
+    return HeaderValidateResponse(
+        ok=len(required_headers) == 0, missing=required_headers
+    )
 
 
 def is_csv(data: TextIO) -> bool:
