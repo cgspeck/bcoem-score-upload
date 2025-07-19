e3caf5100c270954a2b6f2c8ed5621ee0911335a
Chris Speck <cgspeck@gmail.com>
2025-07-19 22:29:21 +1000

    Identify where sorted by style

--- src/controllers/pullsheets.py
+++ src/controllers/pullsheets.py
@@ -1,12 +1,17 @@
 from typing import List, Optional
 from flask import Blueprint, current_app, render_template, request
 
-from src.controllers.helpers import db_config_for_env_shortname, get_db_connection, message_log
+from src.controllers.helpers import (
+    db_config_for_env_shortname,
+    get_db_connection,
+    message_log,
+)
 from src.models.pullsheets import get_data
 from src.utils import must_have_valid_compenv, topt_or_authorized
 from mysql.connector import MySQLConnection
 
 pullsheets = Blueprint("pullsheets", __name__, template_folder="templates")
+
 
 @pullsheets.before_request
 @topt_or_authorized
@@ -18,10 +23,13 @@ def before_request() -> None:
 
 @pullsheets.route("")
 def show() -> str:
-    env_short_name = request.args.get('comp_env', 'prod')
-    env_full_name = [x[1] for x in current_app.config["BCOME_ENV_CHOICES"] if x[0] == env_short_name][0]
+    env_short_name = request.args.get("comp_env", "prod")
+    env_full_name = [
+        x[1] for x in current_app.config["BCOME_ENV_CHOICES"] if x[0] == env_short_name
+    ][0]
 
-    sort_by = request.args.get('sort_by')
+    sort_by_arg = request.args.get("sort_by")
+    sort_by_str = " (sort by entry number )"
 
     messages: List[str] = []
     db_config = db_config_for_env_shortname(env_short_name, messages)
@@ -35,8 +43,14 @@ def show() -> str:
 
     tables = get_data(cnn)
 
-    if sort_by is not None and sort_by == "style":
+    if sort_by_arg is not None and sort_by_arg == "style":
+        sort_by_str = " ( sort by style )"
         for judging_table in tables:
             judging_table.entries.sort(key=lambda e: e.brewstyle_num_int)
 
-    return render_template(f"pullsheets.html", env_full_name=env_full_name, tables=tables)
+    return render_template(
+        f"pullsheets.html",
+        env_full_name=env_full_name,
+        tables=tables,
+        sort_by_str=sort_by_str,
+    )
